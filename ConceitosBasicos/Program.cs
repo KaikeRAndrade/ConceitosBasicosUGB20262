81a5301833c1c1acd4239f5e610a41372aaa5fed
KaikeRAndrade <kaikegg1@gmail.com>
2026-02-24 20:52:59 -0300

    Tratando entrada da idade

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,13 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int idade;
-Console.WriteLine("Digite a sua idade: ");
-idade = Convert.ToInt32(Console.ReadLine());
-if (idade >= 18)
+bool idadevalida = false;
+while(!idadevalida)
 {
-    Console.WriteLine("Você é maior de idade");
+    Console.WriteLine("Digite a sua idade: ");
+    try
+    {
+        idade = Convert.ToInt32(Console.ReadLine());
+        idadevalida = true;
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade");
 
-}
-else
-{
-    Console.WriteLine("Você é menor de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Você é menor de idade.");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("O valor informado deve ser em algarismos algébrico. Números");
+    }
+
+
 }

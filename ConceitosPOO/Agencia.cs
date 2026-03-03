a113fd0e83f22bbf6a3f1741d225a9a47bb54132
KaikeRAndrade <kaikegg1@gmail.com>
2026-03-03 20:46:15 -0300

    Adicionando mais uma classe com o nome Agência, com nome, numero e telefone

---
+++ ConceitosPOO/Agencia.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosPOO
+{
+    public class Agencia
+    {
+        private string _numero;
+        private string _nome;
+        private string _telefone;
+        public string Numero
+        {
+            get
+            {
+                return _numero;
+            }
+            set
+            {
+                if (value.Length != 0)
+                    _numero = value;
+                else
+                    throw new Exception("O numero não pode estar vazio");
+            }
+        }
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value.Length != 0)
+                    _nome = value;
+                else
+                    throw new Exception("Nome da agência não pode estar vazio");
+            }
+        }
+        public string Telefone
+        {
+            get
+            {
+                return _telefone;
+            }
+            set
+            {
+                if (value.Length != 0)
+                    _telefone = value;
+                else
+                    throw new Exception("O numero de telefone não pode estar vazio");
+            }
+        }
+    }
+}
